370ffa8b3466640535e7554032df0ea96eaf2fd5
Shashwat Khanna <shashwatkhanna312@gmail.com>
2021-09-12 00:30:47 +0530

    completed generated website

--- src/GeneratedWebsite/Banner.js
+++ src/GeneratedWebsite/Banner.js
@@ -1,17 +1,20 @@
 import styles from "./Banner.module.css";
 const Banner = ({ theme }) => {
   const theme_name =
-    theme === "light"
-      ? "banner-text-light"
-      : "banner-text-dark";
+    theme === "light" ? "banner-text-light" : "banner-text-dark";
+  const images = [
+    "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2015/02/ctXFWZl-1.gif",
+    "https://i.pinimg.com/originals/18/93/c0/1893c0c89d144588b9be9844da573af1.gif",
+    "http://24.media.tumblr.com/e5e81d2e65d72d761ee6cd5eaec74735/tumblr_mvr3y5KL931rnq3cto1_500.gif",
+    "https://c.tenor.com/f2Dz0QKQlpoAAAAM/time-traveling-its-rucka.gif",
+  ];
+  const image = images[Math.floor(Math.random() * images.length)];
   return (
     <div className={styles["banner"]}>
-      <h2 className={`${styles['banner-text']} ${styles[theme_name]}`}>We present to you - THE PAST</h2>
-      <img
-        src="https://www.heavensgate.com/img/hbhgtd.jpg"
-        className={styles["banner-image"]}
-        alt=""
-      />
+      <h2 className={`${styles["banner-text"]} ${styles[theme_name]}`}>
+        We present to you - THE PAST
+      </h2>
+      <img src={`${image}`} className={styles["banner-image"]} alt="" />
     </div>
   );
 };
--- src/GeneratedWebsite/Body.js
+++ src/GeneratedWebsite/Body.js
@@ -1,10 +1,9 @@
 import styles from "./Body.module.css";
-const Body = ({ theme , value}) => {
-  const theme_name =
-    theme === "light"
-      ? "body-light"
-      : "body-dark";
-  return <div className={`${styles['body']} ${styles[theme_name]}`}>{value}</div>;
+const Body = ({ theme, value }) => {
+  const theme_name = theme === "light" ? "body-light" : "body-dark";
+  return (
+    <div className={`${styles["body"]} ${styles[theme_name]}`}>{value}</div>
+  );
 };
 
 export default Body;
